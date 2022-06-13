b9b6630f67b0954cf1646efb8545c1c1d5d7150d
tschoemaker <you@example.com>
2022-06-13 17:59:18 +0200

    added embedded output

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 GRADLE_VERSION=7.4.1
-curl -O https://downloads.gradle-dn.com/distributions/gradle-${GRADLE_VERSION}-bin.zip
-unzip  gradle-${GRADLE_VERSION}-bin.zip && rm  gradle-${GRADLE_VERSION}-bin.zip
-GRADLE_HOME=/gradle-${GRADLE_VERSION}/bin
-ENV PATH=${GRADLE_HOME}:$PATH
+if [ ! -e gradle-${GRADLE_VERSION} ];then
+    curl -O https://downloads.gradle-dn.com/distributions/gradle-${GRADLE_VERSION}-bin.zip
+    unzip  gradle-${GRADLE_VERSION}-bin.zip && rm  gradle-${GRADLE_VERSION}-bin.zip
+fi
+GRADLE_HOME=/home/compf/Documents/hibernate/HibernateDemo/gradle-${GRADLE_VERSION}/bin
+export PATH=${GRADLE_HOME}:$PATH
+echo $PATH
