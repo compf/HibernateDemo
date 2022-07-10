1843aed278c9f3f90c79f513c29d3c9daa4e68b4
Your Name <you@example.com>
2022-07-10 19:10:30 +0200

    modified checout

--- checkout.sql
+++ checkout.sql
@@ -16,18 +16,29 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
-<<<<<<< HEAD
-=======
-ALTER TABLE ONLY public.student DROP CONSTRAINT fk3bkmp4kohbl54m14tb2fcqya3;
->>>>>>> 8e1b7fb63699b9d390160470ea08e17476da74f8
-ALTER TABLE ONLY public.student DROP CONSTRAINT student_pkey;
-ALTER TABLE ONLY public.person DROP CONSTRAINT person_pkey;
-ALTER TABLE ONLY public.address DROP CONSTRAINT address_pkey;
-DROP TABLE public.student;
-DROP SEQUENCE public.person_seq;
-DROP TABLE public.person;
-DROP SEQUENCE public.address_seq;
-DROP TABLE public.address;
+DROP DATABASE demodb;
+--
+-- Name: demodb; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE demodb WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE = 'German_Germany.1252';
+
+
+ALTER DATABASE demodb OWNER TO postgres;
+
+\connect demodb
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
@@ -92,29 +103,6 @@ CREATE SEQUENCE public.person_seq
 ALTER TABLE public.person_seq OWNER TO postgres;
 
 --
--- Name: student; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.student (
-<<<<<<< HEAD
-    id integer NOT NULL,
-    housenr character varying(255),
-    postcode character varying(255),
-    street character varying(255),
-    birthdate date,
-    firstname character varying(255),
-    lastname character varying(255),
-    matrnumber integer NOT NULL
-=======
-    matrnumber integer NOT NULL,
-    id integer NOT NULL
->>>>>>> 8e1b7fb63699b9d390160470ea08e17476da74f8
-);
-
-
-ALTER TABLE public.student OWNER TO postgres;
-
---
 -- Data for Name: address; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
@@ -129,40 +117,14 @@ COPY public.address (id, housenr, postcode, street) FROM stdin;
 --
 
 COPY public.person (id, housenr, postcode, street, birthdate, firstname, lastname) FROM stdin;
-1	9	49824	Hauptweg	2022-07-10	Gerda	Musterfrau
-<<<<<<< HEAD
 \.
 
 
 --
--- Data for Name: student; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.student (id, housenr, postcode, street, birthdate, firstname, lastname, matrnumber) FROM stdin;
-2	6	49080	Jahnplatz	1997-12-03	Timo	Schoemaker	978621
-=======
-2	6	49080	Jahnplatz	1997-12-03	Timo	Schoemaker
->>>>>>> 8e1b7fb63699b9d390160470ea08e17476da74f8
-\.
-
-
---
-<<<<<<< HEAD
-=======
--- Data for Name: student; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.student (matrnumber, id) FROM stdin;
-978621	2
-\.
-
-
---
->>>>>>> 8e1b7fb63699b9d390160470ea08e17476da74f8
 -- Name: address_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.address_seq', 51, true);
+SELECT pg_catalog.setval('public.address_seq', 1, false);
 
 
 --
@@ -186,14 +148,6 @@ ALTER TABLE ONLY public.address
 
 ALTER TABLE ONLY public.person
     ADD CONSTRAINT person_pkey PRIMARY KEY (id);
-
-
---
--- Name: student student_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.student
-    ADD CONSTRAINT student_pkey PRIMARY KEY (id);
 
 
 --
