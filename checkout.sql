1ec06d2d850a180227f95e547cc4d88c4ed990b8
Your Name <you@example.com>
2022-07-10 18:29:36 +0200

    finnished table-per-class

--- checkout.sql
+++ checkout.sql
@@ -16,12 +16,45 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+ALTER TABLE ONLY public.student DROP CONSTRAINT student_pkey;
 ALTER TABLE ONLY public.person DROP CONSTRAINT person_pkey;
+ALTER TABLE ONLY public.address DROP CONSTRAINT address_pkey;
+DROP TABLE public.student;
 DROP SEQUENCE public.person_seq;
 DROP TABLE public.person;
+DROP SEQUENCE public.address_seq;
+DROP TABLE public.address;
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
+
+--
+-- Name: address; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.address (
+    id integer NOT NULL,
+    housenr character varying(255),
+    postcode character varying(255),
+    street character varying(255)
+);
+
+
+ALTER TABLE public.address OWNER TO postgres;
+
+--
+-- Name: address_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.address_seq
+    START WITH 1
+    INCREMENT BY 50
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.address_seq OWNER TO postgres;
 
 --
 -- Name: person; Type: TABLE; Schema: public; Owner: postgres
@@ -57,6 +90,34 @@ CREATE SEQUENCE public.person_seq
 ALTER TABLE public.person_seq OWNER TO postgres;
 
 --
+-- Name: student; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.student (
+    id integer NOT NULL,
+    housenr character varying(255),
+    postcode character varying(255),
+    street character varying(255),
+    birthdate date,
+    firstname character varying(255),
+    lastname character varying(255),
+    matrnumber integer NOT NULL
+);
+
+
+ALTER TABLE public.student OWNER TO postgres;
+
+--
+-- Data for Name: address; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.address (id, housenr, postcode, street) FROM stdin;
+1	9	49824	Hauptweg
+2	6	49080	Jahnplatz
+\.
+
+
+--
 -- Data for Name: person; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
@@ -67,10 +128,34 @@ Student	2	6	49080	Jahnplatz	1997-12-03	Timo	Schoemaker	978621
 
 
 --
+-- Data for Name: student; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.student (id, housenr, postcode, street, birthdate, firstname, lastname, matrnumber) FROM stdin;
+2	6	49080	Jahnplatz	1997-12-03	Timo	Schoemaker	978621
+\.
+
+
+--
+-- Name: address_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.address_seq', 51, true);
+
+
+--
 -- Name: person_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
 SELECT pg_catalog.setval('public.person_seq', 51, true);
+
+
+--
+-- Name: address address_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.address
+    ADD CONSTRAINT address_pkey PRIMARY KEY (id);
 
 
 --
@@ -79,6 +164,14 @@ SELECT pg_catalog.setval('public.person_seq', 51, true);
 
 ALTER TABLE ONLY public.person
     ADD CONSTRAINT person_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: student student_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.student
+    ADD CONSTRAINT student_pkey PRIMARY KEY (id);
 
 
 --
